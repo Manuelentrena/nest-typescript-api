0682c2f7bd4740fcb926a320d49c914f482c45b1
EntrenaDev <manuel.entrena@gmail.com>
2024-11-05 20:43:59 +0100

    feat: :sparkles: Added repository user

---
+++ src/user/domain/repositories/UserRepository.ts
@@ -0,0 +1,11 @@
+import { User } from '../entities/User';
+import { UserEmail } from '../value-objects';
+import { UserId } from '../value-objects/UserId';
+
+export interface UserRepository {
+  register(user: User): Promise<User | null>;
+  login(email: UserEmail, password: string): Promise<User | null>;
+  findById(id: UserId): Promise<User | null>;
+  resetPassword(userId: UserId, newPassword: string): Promise<User | null>;
+  logout(userId: UserId): Promise<void>;
+}
