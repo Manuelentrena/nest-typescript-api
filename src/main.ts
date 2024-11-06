6ccd94762334517f74528037ef8719c502435adf
EntrenaDev <manuel.entrena@gmail.com>
2024-11-06 18:38:43 +0100

    feat:
    ç

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { AllExceptionsFilter } from './config/allExceptionsFilter';
+import { AllExceptionsFilter } from './shared/exceptions/allExceptionsFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
